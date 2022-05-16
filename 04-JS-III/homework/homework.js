408874143baee7db735441ad2ea5e15b2392ab0b
ramirogz1 <ramirogz101@gmail.com>
2022-05-16 14:39:00 -0300

    act js3

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -10,12 +10,14 @@ return array [0];
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array[array.length-1];
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length;
 }
 
 
