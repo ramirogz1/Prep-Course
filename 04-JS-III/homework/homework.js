2831fc306d4efde704c84394e5a4fbff04bd94da
ramirogz1 <ramirogz101@gmail.com>
2022-05-16 19:07:16 -0300

    act js3

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -125,6 +125,14 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if (arguments.length === 0){
+    return 0;
+  }
+  var x =1;
+  for(i=0 ; i< arguments.length ; i++){
+    x=x*arguments[i];
+  }
+  return x ;
 }
 
 
