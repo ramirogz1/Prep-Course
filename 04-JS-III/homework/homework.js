99ccf815771d063f0ed1de3d06c4d274ab5475dd
ramirogz1 <ramirogz101@gmail.com>
2022-05-16 20:45:36 -0300

    act js3

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -139,6 +139,13 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
+  cont = 0;
+  for (i=0 ;i<arreglo.length ;i++){
+    if(arreglo[i]>18){
+      cont++;
+    }
+  }
+  return cont
 
 }
 
