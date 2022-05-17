c027f1d10bfe0c419126296583a8d82b037d110d
ramirogz1 <ramirogz101@gmail.com>
2022-05-17 02:12:36 -0300

    act js3

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -239,6 +239,25 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var nuevo = [];
+    for (i=0;i<10;i++){
+      numero = numero + 2 ;
+      nuevo.push(numero);
+      if (numero === i){
+        break
+       
+      }
+      
+
+    }
+    if (i===10){
+      return nuevo
+    }
+    else {
+      return "Se interrumpió la ejecución";
+    }
+    
+
 }
 
 
@@ -249,6 +268,21 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+var nuevo = [];
+for (i=0 ; i<10;i++){
+  if (i===5){
+    continue;
+  }
+  numero= numero+2;
+  nuevo.push(numero);
+  
+
+  
+
+
+}
+return nuevo;
+
 }
 
 
