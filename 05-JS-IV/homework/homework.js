6ce844a3f37fffe0a607686125d6532ea2155499
ramirogz1 <ramirogz101@gmail.com>
2022-05-20 02:55:59 -0300

    finish homework IV

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -101,9 +101,12 @@ function verificarPassword (usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (usuario.password === password){
-    return true ;
-  }
+  //if (usuario["password"] === password){
+    if (usuario.password === password){
+      return true ;
+    }
+    
+  
   else {
     return false;
   }
