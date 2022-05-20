4413fd86ce11014ab335faae740404e76647212b
ramirogz1 <ramirogz101@gmail.com>
2022-05-20 02:52:08 -0300

    finish homework IV

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -60,13 +60,26 @@ function nuevoUsuario (nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
+   var user = {
+     nombre : nombre ,
+     email : email,
+     password: password,
+   }
+   return user;
+   
 
 }
 
 function tieneEmail (usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
-  // Tu código:
+  // Tu código: 
+  if (usuario.email ){
+    return true
+  }
+  else {
+    return false
+  }
 }
 
 function tienePropiedad (objeto, propiedad) {
@@ -74,6 +87,13 @@ function tienePropiedad (objeto, propiedad) {
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (objeto[propiedad]){
+    return true;
+  }
+  else {
+    return false;
+  }
+
 }
 
 function verificarPassword (usuario, password) {
@@ -81,6 +101,12 @@ function verificarPassword (usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (usuario.password === password){
+    return true ;
+  }
+  else {
+    return false;
+  }
 }
 
 function actualizarPassword (usuario, nuevaPassword) {
