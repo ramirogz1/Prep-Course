f2ccfeaca9047cee697aac778363e72f30497cc5
ramirogz1 <ramirogz101@gmail.com>
2022-05-20 04:08:55 -0300

    finish homework IV

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -169,7 +169,7 @@ function agregarMetodoCalculoDescuento (producto) {
   producto.calcularPrecioDescuento = function () {
        return this.precio - (this.precio * this.porcentajeDeDescuento);
   }
-  return producto;
+  return producto; 
 
 }
 
